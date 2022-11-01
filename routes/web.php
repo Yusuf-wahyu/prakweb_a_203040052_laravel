1dff3e53db06c49751c2631a574811378443d5a1
Yusuf-wahyu <yusufwahyusyahroni@gmail.com>
2022-11-01 18:01:45 +0700

    menyelesaikan video 4 blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,71 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view ('home');
+    return view ('home', [
+    "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view ('about', [
+    "title" => "About",
     "name" => "Yusuf Wahyu Syahroni",
     "email" => "yuswhys@gmail.com",
     "image" => "yusuf.jpg"
     ]);
 });
 
+
 Route::get('/blog', function () {
-    return view ('posts');
+    $blog_posts =[
+        [
+        "title" => "Judul Post Pertama",
+        "slug" => "Judul-post-pertama",
+        "author" => "Yusuf Wahyu Syahroni",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, alias? Nobis, neque! Vero nobis iure voluptatem aspernatur laboriosam, quibusdam veniam atque ab. Ad reprehenderit odit itaque enim molestiae fugit tenetur."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "Judul-post-kedua",
+            "author" => "ucup",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, alias? Nobis, neque! Vero nobis iure voluptatem aspernatur laboriosam, quibusdam veniam atque ab. Ad reprehenderit odit itaque enim molestiae fugit tenetur."
+        ],
+    ];
+    return view ('posts', [
+    "title" => "Posts",
+    "posts" => $blog_posts
+    ]);
+});
+
+
+// halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts =[
+        [
+        "title" => "Judul Post Pertama",
+        "slug" => "Judul-post-pertama",
+        "author" => "Yusuf Wahyu Syahroni",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, alias? Nobis, neque! Vero nobis iure voluptatem aspernatur laboriosam, quibusdam veniam atque ab. Ad reprehenderit odit itaque enim molestiae fugit tenetur."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "Judul-post-kedua",
+            "author" => "ucup",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, alias? Nobis, neque! Vero nobis iure voluptatem aspernatur laboriosam, quibusdam veniam atque ab. Ad reprehenderit odit itaque enim molestiae fugit tenetur."
+        ],
+    ];
+
+
+    $new_post;
+    foreach($blog_posts as $post){
+        if($post['slug'] === $slug) {
+            $new_post = $post;
+
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
