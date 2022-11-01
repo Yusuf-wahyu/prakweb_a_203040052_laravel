eaf581c8e8681940cc29d636bf11a8d72c77a9b2
Yusuf-wahyu <yusufwahyusyahroni@gmail.com>
2022-11-01 15:32:35 +0700

    menyelesaikan video ke 3 struktur folder, route & view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('home');
+});
+
+Route::get('/about', function () {
+    return view ('about', [
+    "name" => "Yusuf Wahyu Syahroni",
+    "email" => "yuswhys@gmail.com",
+    "image" => "yusuf.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view ('posts');
 });
