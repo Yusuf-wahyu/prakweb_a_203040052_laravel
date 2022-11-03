0c2da628c2cc890aa98a03a0d111d5f0717d675d
Yusuf-wahyu <yusufwahyusyahroni@gmail.com>
2022-11-03 22:06:42 +0700

    menyelesaikan video ke 7 Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,33 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-        "title" => "Judul Post Pertama",
-        "slug" => "Judul-post-pertama",
-        "author" => "Yusuf Wahyu Syahroni",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, alias? Nobis, neque! Vero nobis iure voluptatem aspernatur laboriosam, quibusdam veniam atque ab. Ad reprehenderit odit itaque enim molestiae fugit tenetur."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "Judul-post-kedua",
-            "author" => "ucup",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, alias? Nobis, neque! Vero nobis iure voluptatem aspernatur laboriosam, quibusdam veniam atque ab. Ad reprehenderit odit itaque enim molestiae fugit tenetur."
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-            return $posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded =['id'];
 }
