552e74884dede3eb67e11cd38d39b07c98bf66c3
Yusuf-wahyu <yusufwahyusyahroni@gmail.com>
2022-11-25 22:24:59 +0700

    menyelesaikan video 22 Preview, Update & Delete Image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 
 
@@ -102,17 +103,25 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image'=> 'image|file|max:2024',
             'body' => 'required',
         ];
+
 
         if($request->slug != $post->slug){
             $rules['slug'] = 'required|unique:post';
         }
 
         $validateData = $request->validate($rules);
+        if($request->file('image')) {
+            if($request->oldImage){
+                Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
 
-        $validateData['user_id'] = auth()->user()->id;
-        $validateData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
 
         Post::where('id', $post->id)
             ->update($validatedData);
@@ -128,6 +137,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if($post->image){
+            Storage::delete($post->image);
+        }
         Post::destroy($post->id);
 
         return redirect('/dashboard/posts')->with('success', 'Jeng Jrengg Post nya udah di hapus');
